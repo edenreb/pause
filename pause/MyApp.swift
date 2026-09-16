f36f2ee7735bb36bd920370e51a497fe4d3d63b0
edenreb <edenvrebello@gmail.com>
2026-09-16 16:50:32 +0400

    Working menu bar and timer

--- pause/MyApp.swift
+++ pause/MyApp.swift
@@ -1,29 +1,90 @@
 import SwiftUI
+import Combine
+import Foundation
 
-struct MyApp: App {
+class BreakManager: ObservableObject {
     
-    // Telling macOS to not show a standard app or put it in the dock.
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    // Setting up default values for pauses
+    @Published var selectedDuration: Int = 30
+    @Published var secondsRemaining: Int = 30 * 60
+    @Published var isBreakActive: Bool = false
+    
+    private var timer: Timer?
+    
+    func startTimer() {
+        // Stop existing timer so I don't have duplicates
+        stopTimer()
+        
+        secondsRemaining = selectedDuration * 60
+        
+        // Preventing memory leaks by
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self]
+            i in guard let self = self else { return }
+            
+            // Countdown to 0 by decrementing secondsRemaining by 1 every second
+            if self.secondsRemaining > 0 {
+                self.secondsRemaining -= 1
+            }
+            
+            // Trigger the pause if 0 seconds remaining
+            else {
+                self.triggerPause()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func triggerPause() {
+        stopTimer()
+        isBreakActive = true
+        
+        // TODO: Launch pause
+        PauseView()
+    }
+}
+
+@main struct MyApp: App {
+    
+    @StateObject private var breakManager = BreakManager()
     
     var body: some Scene {
         
-        // Telling macOS to not show a blank window on startup
-        Settings {
-            EmptyView()
+        // Setting up the menu bar
+        MenuBarExtra("Pause", systemImage: "heart.badge.bolt") {
+            
+            // Creating a "dropdown" list for changing the duration between pauses
+            Picker("Gap Duration", selection: $breakManager.selectedDuration) {
+                Text("20 minutes").tag(20)
+                Text("30 minutes").tag(30)
+                Text("40 minutes").tag(40)
+            }
+            .pickerStyle(.inline)
+            .onChange(of: breakManager.selectedDuration) {
+                breakManager.startTimer()
+            }
+            
+            Divider()
+            
+            Button("Pause now") {
+                breakManager.triggerPause()
+            }
+            
+            Divider()
+            
+            // The only way to quit the app
+            Button("Quit App"){
+                NSApplication.shared.terminate(nil)
+            }
+        }
+        
+        WindowGroup(id: "pauseWindow") {
+            PauseView()
+                .environmentObject(breakManager)
         }
     }
 }
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var statusItem: NSStatusItem?
-    
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.prohibited)
-        
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
-        if let button = statusItem?.button {
-            button.image = NSImage(systemSymbolName: "star.fill" accessibilityDescription: "Pause.")
-        }
-    }
-}
---
+++ pause/PauseView.swift
@@ -0,0 +1,18 @@
+//
+//  PauseView.swift
+//  pause
+//
+//  Created by Eden Rebello on 9/16/26.
+//
+
+import SwiftUI
+
+struct PauseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PauseView()
+}
